9d1ea97efcb5b2e4724da6fd82b5a6dd855e3ae5
LE DUC TAM <giaythuytinh176@gmail.com>
2020-12-13 01:19:22 +0700

    translator

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get("/translator", function () {
+    return view("translator");
+});
+
+Route::post("/translator", function (\Illuminate\Http\Request $request) {
+
+    $list = [
+        "english" => "tieng anh",
+        "hello" => "xin chao",
+    ];
+    if ($request->translate && array_key_exists($request->translate, $list) == true) {
+        return view("translator", ["meaning" => $list[$request->translate]]);
+    }
+});
+
+
+Route::get("/contact", "\App\Http\Controllers\ContactController@ShowContact");
+
+
+Route::post("/contact", "\\App\\Http\\Controllers\\ContactController@InsertMess");
